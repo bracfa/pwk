f448f1b1c6c7e2fc8a5647640e714d7742dc02df
Christine Bradshaw <bradshawcfa@gmail.com>
2020-08-05 07:19:35 -0400

    Getting targets from scans

--- scripts/hosts/identifyTargets.sh
+++ scripts/hosts/identifyTargets.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 # Enumerate the targets from all the scripts
-
-# Identify all XML files
-FNAMES=$(ls /home/kali/gitWorkspace/pwk/outputFiles/hosts/pwk/*.xml)
 RESDIR="/home/kali/gitWorkspace/pwk/results/pwk/"
+OUTDIR="/home/kali/gitWorkspace/pwk/outputFiles/hosts/pwk/"
+
+# Go to the results directory
+cd "$OUTDIR"
+
+# Identify all XML files. This not yet an array!
+FNAMES=$(ls *.xml)
+echo "FNAMES= $FNAMES"
+sleep 10
+SAVEIFS=$IFS
+IFS=$'\n'
+FARRAY=($FNAMES)
+IFS=$SAVEIFS
+
 
 # Create a file for each xml file which shows all the hosts
-for file in "${FNAMES[@]}"; do
-    # Get just the filename
-    temp=$(echo "$file" | awk -F'/' '{ print $NF }' | awk -F'.xml' '{ print $1 }')
-    echo "$temp""res"
+for file in "${FARRAY[@]}"
+do
+    echo "FILENAME: $file"
+    parse-nmap "$file"
 done
 
 # Add all the hosts to one file and make sure every target is unique
